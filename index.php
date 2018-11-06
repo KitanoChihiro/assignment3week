f0c54c3609a20d3422a1a40f9bd71577987e0a16
北野千裕 <chi.t.angel.s2@gmail.com>
2018-11-06 18:20:38 +0800

    投稿一覧表示BOX作成

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     </head>
     <body>
         <div class="header">
-            <div class="header-logo">NexSeed Diary</div>
+            <div class="header-logo" style="font-family: arial;">NexSeed Diary</div>
         </div>
         
         <div>
@@ -18,8 +18,12 @@
             <p class="topic">2018年9月の日記</p>
         </div>
 
-
-
+        <div class="box">
+            <a href=""><p style="font-size: 40px; padding-left: 20px;">こんにちは</p></a>
+            <p style="padding-left: 20px;">2018年11月6日</p>
+        </div>
+        
+        <br>
 
 
 

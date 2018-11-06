65409e420544b94dfb10a3bf8ec95a45c6a68de5
北野千裕 <chi.t.angel.s2@gmail.com>
2018-11-06 18:01:54 +0800

    ヘッダーとフッダーの作成

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>NexSeed</title>
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+        <div class="header">
+            <div class="header-logo">NexSeed Diary</div>
+        </div>
+        
+        <div>
+            <p class="topic">こんにちはゲストさん</p>
+            <br>
+            <p class="topic">2018年11月の日記</p>
+            <p class="topic">2018年10月の日記</p>
+            <p class="topic">2018年9月の日記</p>
+        </div>
+
+
+
+
+
+
+
+
+        <div class="footer">
+            <div class="footer-logo">NexSeed</div>
+        </div>
+    </body>
+</html>

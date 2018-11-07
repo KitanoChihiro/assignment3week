007e5dca5b41e1b641be91c0484d47285b82ae21
北野千裕 <chi.t.angel.s2@gmail.com>
2018-11-07 15:57:38 +0800

    コンにちは機能追加

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<?php 
+
+date_default_timezone_set('Asia/Tokyo');
+$time = intval(date('G:i'));
+$day = $time;
+
+if (0600 <= $time && $time<= 1100) {
+    $day = 'おはようございます、ゲストさん';
+}elseif (1101 < $time && $time< 1759) {
+    $day = 'こんにちは、ゲストさん';
+}else{
+    $day = 'こんばんは、ゲストさん';
+}
+
+
+
+
+ ?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="ja">
     <head>
@@ -11,7 +33,7 @@
         </div>
         
         <div>
-            <p class="topic">こんにちはゲストさん</p>
+            <p class="topic"><?php echo $day ?></p>
             <br>
             <a href=""><p class="topic">2018年11月の日記</p></a>
         </div>

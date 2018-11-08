370d9e3c5972a635d6ff720656e132be960c16e2
北野千裕 <chi.t.angel.s2@gmail.com>
2018-11-08 14:42:57 +0800

    左に過去３ヶ月の月を表示した

--- index.php
+++ index.php
@@ -1,24 +1,18 @@
 <?php 
 
 date_default_timezone_set('Asia/Tokyo');
-$time = intval(date('G:i'));
+$time = intval(date('H:i'));
 $day = $time;
 
-if (0600 <= $time && $time<= 1100) {
+if ('06:00' <= $time && $time<= '11:00') {
     $day = 'おはようございます、ゲストさん';
-}elseif (1101 < $time && $time< 1759) {
+}elseif ('11:01' < $time && $time< '17:59') {
     $day = 'こんにちは、ゲストさん';
 }else{
     $day = 'こんばんは、ゲストさん';
 }
 
-
-
-
  ?>
-
-
-
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -29,24 +23,43 @@ if (0600 <= $time && $time<= 1100) {
     </head>
     <body>
         <div class="header">
-            <div class="header-logo" style="font-family: arial;">NexSeed Diary</div>
+        <div class="header-logo" style="font-family: arial;">NexSeed Diary</div>
         </div>
         
-        <div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <div style="width: 100px;">
             <p class="topic"><?php echo $day ?></p>
-            <br>
-            <a href=""><p class="topic">2018年11月の日記</p></a>
+            <a href=""><p class = "month" ><?php echo date('Y' . '年' . 'm' . '月の日記', strtotime("now")); ?></p></a>
+            <a href=""><p class="month"><?php echo date('Y' . '年' . 'm' . '月の日記', strtotime("-1 month")); ?></p></a>
+            <a href=""><p class="month"><?php echo date('Y' . '年' . 'm' . '月の日記', strtotime("-2 month")); ?></p></a>
         </div>
 
         <div class="box">
             <a href=""><p style="font-size: 40px; padding-left: 20px;">こんにちは</p></a>
             <p style="padding-left: 20px;">2018年11月6日</p>
         </div>
+
+        <div class="box">
+            <a href=""><p style="font-size: 40px; padding-left: 20px;">こんにちは</p></a>
+            <p style="padding-left: 20px;">2018年11月6日</p>
+        </div>
+
+        <div class="box">
+            <a href=""><p style="font-size: 40px; padding-left: 20px;">こんにちは</p></a>
+            <p style="padding-left: 20px;">2018年11月6日</p>
+        </div>
+
+        <div class="box">
+            <a href=""><p style="font-size: 40px; padding-left: 20px;">こんにちは</p></a>
+            <p style="padding-left: 20px;">2018年11月6日</p>
+        </div>
+
+
         
         <br>
-
-
-
 
 
         <div class="footer">

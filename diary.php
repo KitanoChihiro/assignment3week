c328ee475215f18cb1be696144ef3f562e4a3559
北野千裕 <chi.t.angel.s2@gmail.com>
2018-11-09 00:31:24 +0800

    ダイアリー入力フォームの作成

---
+++ diary.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Diary</title>
+    <link rel="stylesheet" href="diary.css">
+</head>
+<body>
+    <div class="header">
+        <h1 class="header-logo">NexSeed Diary</h1>
+    </div>
+
+    <div class="form">
+        <form action="" method="POST">
+            <fieldset class="field">
+                <h2>title</h2>
+                <input type="text" class="text"><br>
+                <h2>diary</h2>
+                <textarea class="textarea"></textarea>
+                <div class="submit">
+                    <a href=""><input type="submit" class="btn" value="投稿！！" style="font-size: 15px;"></a>
+                    <a href="index.php"><input type="submit" class="btn" value="戻る" style="font-size: 15px;"></a>
+                </div>
+
+            </fieldset>
+        </form>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class="footer">
+        <p class="footer-logo">NexSeed</p>
+    </div>
+</body>
+</html>

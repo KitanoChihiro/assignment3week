306bbb37f779b691b552ecdcb020b0bbfb8c2967
北野千裕 <chi.t.angel.s2@gmail.com>
2018-11-13 14:29:31 +0800

    DBにコンテンツの登録処理（INSERT）

--- diary.php
+++ diary.php
@@ -1,8 +1,45 @@
 
+<?php 
+
+    require_once('dbconnect.php');
+
+    $id = '';
+    $title = '';
+    $contents = '';
+
+    // 投稿のするためのif文
+    $errors = [];
+
+    if (!empty($_POST)) {
+        
+        $title = $_POST['title'];
+        $contents = $_POST['contents'];
+
+    if ($title == '') {
+        $errors['title'] = '空';
+    }
+    if ($contents == '') {
+        $errors['contents'] = '空';
+    }
+
+    // もしちゃんと全て登録されていたら登録処理
+    if (empty($errors)) {
+
+    // DBにタイトルと本文を登録する
+    $sql = "INSERT INTO `diaries`(`id`, `title`, `contents`, `created`) VALUES (?,?,?,NOW())";
+    $data = [$id, $title, $contents];
+    $stmt = $dbh->prepare($sql);
+    $stmt -> execute($data);
+
+    // header('Location: diary.php');
+    // exit();
+    }
+}
 
 
 
 
+ ?>
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -17,31 +54,19 @@
     </div>
 
     <div class="form">
-        <form action="index.php" method="POST">
+        <form action="diary.php" method="POST">
             <fieldset class="field">
                 <h2>title</h2>
-                <input type="text" class="text"><br>
+                <input  type="text" name="title" class="text" placeholder="タイトルの入力"><br>
                 <h2>diary</h2>
-                <textarea class="textarea"></textarea>
-                <div class="submit">
-                    <a href=""><input type="submit" class="btn" value="投稿！！" style="font-size: 15px;"></a>
-                    <input type="submit" class="btn" value="戻る" style="font-size: 15px;"></a>
-                </div>
+                <textarea class="textarea" name="contents" placeholder="本文の入力"></textarea>
+                <ul class="submit">
+                    <a href="diary.php"><li><button type="submit" class="btn" value="post" style="font-size: 15px;">投稿！</button></li></a>
+                    <a href="index.php"><li type="submit" class="btn" value="" style="font-size: 15px;">戻る</li></a>
+                </ul>
             </fieldset>
         </form>
     </div>
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
